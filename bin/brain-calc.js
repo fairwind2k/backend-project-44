948d17b7ad3c8dbf0981482971fa88d0c5791a93
fairwind2k <fairwind@md2k.net>
2023-07-19 00:00:51 +0200

    refactor code

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -4,13 +4,15 @@
 import { pairs, play } from '../src/index.js';
 
 const gameAsk = 'What is the result of the expression?';
+const min = 0;
+const max = 20;
 
 const getSetConditionAndAnswer = () => {
   const operators = ['+', '-', '*'];
   const size = operators.length;
   const randomIndex = Math.floor(Math.random() * size);
   const symbol = operators[randomIndex];
-  const arr = pairs();
+  const arr = pairs(min, max);
   const expression = `${arr[0]} ${symbol} ${arr[1]}`;
   let res;
   switch (symbol) {
