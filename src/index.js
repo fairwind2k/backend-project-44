582c3b7d76ea51bb93041cc62233190d3d0f72ec
fairwind2k <fairwind@md2k.net>
2023-07-19 00:00:16 +0200

    refactor code

--- src/index.js
+++ src/index.js
@@ -2,25 +2,26 @@ import readlineSync from 'readline-sync';
 
 // Set a limit for random number:
 
-const maxNumber = 10;
+// const minNumber = 0;
+// const maxNumber = 10;
 
 // Set a limit of attempts:
 const attempts = 3;
 
 // Get a random number:
 
-const getRandomInt = (max) => {
-  const randomNumber = Math.floor(Math.random() * max);
+const getRandomInt = (min, max) => {
+  const randomNumber = Math.floor(Math.random() * (max - min) + min);
   return randomNumber;
 };
 
 // get random pairs of numbers:
 
-const pairs = () => {
+const pairs = (min, max) => {
   const array = [];
-  const number1 = getRandomInt(maxNumber);
+  const number1 = getRandomInt(min, max);
   array.push(number1);
-  const number2 = getRandomInt(maxNumber);
+  const number2 = getRandomInt(min, max);
   if (number1 > number2) {
     array.push(number2);
   } else {
