29db1855933e37e5337de750a67bf3bbd977a89f
fairwind2k <fairwind@md2k.net>
2023-07-18 13:43:25 +0200

    refactor core function

--- src/index.js
+++ src/index.js
@@ -29,23 +29,23 @@ const pairs = () => {
   return array;
 };
 
-const play = (gameStr, set) => {
+const play = (gameStr, func) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(gameStr);
   for (let i = 0; i < attempts; i += 1) {
-    const [question, rightAnswer] = set[i];
+    const [question, rightAnswer] = func();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer === rightAnswer) {
       console.log('Correct!');
     } else {
-      return `'${userAnswer}' is wrong answer. Correct answer was '${rightAnswer}'. Let's try again, ${userName}`;
+      return `'${userAnswer}' is wrong answer. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`;
     }
   }
   const finalWords = `Congratulations, ${userName}!`;
   return finalWords;
 };
 
-export { play, pairs };
+export { play, pairs, getRandomInt };
