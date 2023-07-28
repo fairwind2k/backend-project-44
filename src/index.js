c23c929553898205aa3b04da701d3bca1c78157b
fairwind2k <fairwind@md2k.net>
2023-07-28 15:33:25 +0200

    refactor code

--- src/index.js
+++ src/index.js
@@ -1,42 +1,14 @@
 import readlineSync from 'readline-sync';
 
-// Set a limit for random number:
-
-// const minNumber = 0;
-// const maxNumber = 10;
-
-// Set a limit of attempts:
 const attempts = 3;
 
-// Get a random number:
-
-const getRandomInt = (min, max) => {
-  const randomNumber = Math.floor(Math.random() * (max - min) + min);
-  return randomNumber;
-};
-
-// get random pairs of numbers:
-
-const pairs = (min, max) => {
-  const array = [];
-  const number1 = getRandomInt(min, max);
-  array.push(number1);
-  const number2 = getRandomInt(min, max);
-  if (number1 > number2) {
-    array.push(number2);
-  } else {
-    array.unshift(number2);
-  }
-  return array;
-};
-
-const play = (gameStr, func) => {
+const play = (gameQuestion, runCustomGameFunction) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  console.log(gameStr);
+  console.log(gameQuestion);
   for (let i = 0; i < attempts; i += 1) {
-    const [question, rightAnswer] = func();
+    const [question, rightAnswer] = runCustomGameFunction();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer === rightAnswer) {
@@ -49,4 +21,4 @@ const play = (gameStr, func) => {
   return finalWords;
 };
 
-export { play, pairs, getRandomInt };
+export default play;
