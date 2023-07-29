f0b9604e4355cc6debb9ac18f3358adfb7e173f8
fairwind2k <fairwind@md2k.net>
2023-07-29 16:31:01 +0200

    refactor code

--- src/games/calc.js
+++ src/games/calc.js
@@ -5,27 +5,32 @@ const gameAsk = 'What is the result of the expression?';
 const min = 0;
 const max = 20;
 
+const getExpression = (rundomSymbol, numbers) => {
+  let res = 0;
+  switch (rundomSymbol) {
+    case '+':
+      res = numbers[0] + numbers[1];
+      break;
+    case '-':
+      res = numbers[0] - numbers[1];
+      break;
+    case '*':
+      res = numbers[0] * numbers[1];
+      break;
+    default:
+      res = null;
+  }
+  return res;
+};
+
 const getSetConditionAndAnswer = () => {
   const operators = ['+', '-', '*'];
   const size = operators.length;
   const randomIndex = Math.floor(Math.random() * size);
   const symbol = operators[randomIndex];
-  const arr = pairs(min, max);
-  const expression = `${arr[0]} ${symbol} ${arr[1]}`;
-  let res;
-  switch (symbol) {
-    case '+':
-      res = arr[0] + arr[1];
-      break;
-    case '-':
-      res = arr[0] - arr[1];
-      break;
-    case '*':
-      res = arr[0] * arr[1];
-      break;
-    default:
-      res = null;
-  }
+  const rundomNumbers = pairs(min, max);
+  const expression = `${rundomNumbers[0]} ${symbol} ${rundomNumbers[1]}`;
+  const res = getExpression(symbol, rundomNumbers);
   const setQandA = [expression, String(res)];
   return setQandA;
 };
