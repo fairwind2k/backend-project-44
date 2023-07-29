368ce1587d64a428af190447b9dec86e5da8345a
fairwind2k <fairwind@md2k.net>
2023-07-29 15:59:39 +0200

    refactor code

--- src/helpers/pairs.js
+++ src/helpers/pairs.js
@@ -1,16 +1,9 @@
 import getRandomInt from './random_number.js';
 
 const pairs = (min, max) => {
-  const array = [];
   const number1 = getRandomInt(min, max);
-  array.push(number1);
   const number2 = getRandomInt(min, max);
-  if (number1 > number2) {
-    array.push(number2);
-  } else {
-    array.unshift(number2);
-  }
-  return array;
+  return (number1 > number2) ? [number1, number2] : [number2, number1];
 };
 
 export default pairs;
