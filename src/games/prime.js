7211fc5df9c49c8eee2093d2998a2c3c6d3ec6fe
fairwind2k <fairwind@md2k.net>
2023-07-30 20:22:49 +0200

    refactor code

--- src/games/prime.js
+++ src/games/prime.js
@@ -11,12 +11,10 @@ const isPrimeNumber = (num) => {
   if (num < 2) {
     return false;
   }
-  let i = 2;
-  while (i <= num / 2) {
+  for (let i = 2; i < Math.sqrt(num); i += 1) {
     if (num % i === 0) {
       return false;
     }
-    i += 1;
   }
   return true;
 };
